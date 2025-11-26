f3d0cc144f0cbe484d7c1815e1c1d73b2c39f62d
Sittan.r <sittan.rattanaliam@gmail.com>
2025-11-26 22:53:24 +0700

    Initial React + Q1 setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Poke Ex</h1>
+      <p>Explore • Experience • Expert</p>
+    </div>
+  )
+}
+
+export default App
+
